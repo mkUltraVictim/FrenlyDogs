e9d47a6ece57a96ca24e4d7561ce2bbda507b633
mkUltraVictim <85332767+mkUltraVictim@users.noreply.github.com>
2021-12-07 12:51:43 -0500

    Add files via upload

--- FrenlyDogs.sol
+++ FrenlyDogs.sol
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -10,23 +9,30 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable, IERC20, AccessControl {
+contract MyNFT is ERC721URIStorage, Ownable, AccessControl {
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     IERC20 public dog;
-    // Create a new role identifier for the minter role
+    // Create a new role identifier for the whitelisted role
     bytes32 public constant WHITELISTED_ROLE = keccak256("WHITELISTED_ROLE");
-    uint128 mintCost = 13810000000000000000000;
+    uint128 MINTCOST = 6905000000000000000000;
 
-    constructor(address whitelisted) ERC721("FrenlyDogs", "FND") {
-        // Grant the minter role to a specified account
-        _setupRole(WHITELISTED_ROLE, whitelisted);
-        // Grant the contract deployer the default admin role: it will be able
-        // to grant and revoke any roles
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    constructor(address[] memory whitelisted) ERC721("Frenly Dogs", "FND") {
+        // Grant the WHITELISTED_ROLE to all addresses in array 'whitelisted'
+        for (uint i=0; i<whitelisted.length; i++) {
+            address whitelistfinal = whitelisted[i];
+            grantRole(WHITELISTED_ROLE, whitelistfinal);
+        }
     }
 
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Metadata).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+    
     function _baseURI() internal view virtual override returns (string memory) {
         return "ipfs://metadatas_to_be_added/";
     }
@@ -42,21 +48,14 @@ contract MyNFT is ERC721URIStorage, Ownable, IERC20, AccessControl {
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
     }
-    
-    function approval() public onlyRole(WHITELISTED_ROLE) {
-        dog.approve(owner(), mintCost);
-    }
 
     function buy() public onlyRole(WHITELISTED_ROLE) {
         address sender = _msgSender();
-        // Check that the calling account has the whitelisted role
-        require(hasRole(WHITELISTED_ROLE, msg.sender), "Caller is not whitelisted");
-        // Revoke the whitelisted role and then mint.
-        revokeRole(WHITELISTED_ROLE, sender);
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
-        dog.safeTransferFrom(sender, owner(), mintCost);
-        _mint(sender, newItemId); // mints the next token to the address provided
+        dog.safeTransferFrom(sender, owner(), MINTCOST);
+        _safeMint(sender, newItemId); 
+        revokeRole(WHITELISTED_ROLE, sender);
     }
 
 }
