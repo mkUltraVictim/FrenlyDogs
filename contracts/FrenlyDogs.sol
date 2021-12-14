3a0da9a50cfd174140f4c0c66a18898aaebb841e
ComputerSmoke <addison@assurancefamily.com>
2021-12-13 21:41:53 -0600

    removed outdated TODO comment

--- contracts/FrenlyDogs.sol
+++ contracts/FrenlyDogs.sol
@@ -9,7 +9,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "hardhat/console.sol";
-//TODO: Fix 'missing role' on buy
 contract MyNFT is ERC721URIStorage, Ownable {
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
